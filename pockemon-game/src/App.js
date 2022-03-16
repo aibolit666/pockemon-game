2ab3c09a1f74f842eabba02483a720f0abe9fde2
Pavel <pavel.priladyshev@gmail.com>
2022-03-16 18:23:02 +0300

    feat: complete

--- pockemon-game/src/App.js
+++ pockemon-game/src/App.js
@@ -1,170 +1,55 @@
-import Header from "./components/Header";
-import Layout from "./components/Layout";
+import {useRouteMatch, Route, Switch, Redirect } from "react-router-dom";
+import firebase from "firebase";
+import HomePage from "./routes/Home";
+import GamePage from "./routes/Game";
+import MenuHeader from "./components/MenuHeader";
 import Footer from "./components/Footer";
-import urlBG1 from "./Assets/bg1.jpg";
-import urlBG2 from "./Assets/bg3.jpg";
-import logoPng from "./Assets/bg2.jpg";
-import PokemonCard from "./components/PokemonCard";
-import "./App.css";
+import s from './style.module.css'
+import cn from "classnames";
 
-const POKEMONS = [
-  {
-    abilities: ["keen-eye", "tangled-feet", "big-pecks"],
-    stats: {
-      hp: 63,
-      attack: 60,
-      defense: 55,
-      "special-attack": 50,
-      "special-defense": 50,
-      speed: 71,
-    },
-    type: "flying",
-    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/17.png",
-    name: "pidgeotto",
-    base_experience: 122,
-    height: 11,
-    id: 17,
-    values: {
-      top: "A",
-      right: 2,
-      bottom: 7,
-      left: 5,
-    },
-  },
-  {
-    abilities: ["intimidate", "shed-skin", "unnerve"],
-    stats: {
-      hp: 60,
-      attack: 95,
-      defense: 69,
-      "special-attack": 65,
-      "special-defense": 79,
-      speed: 80,
-    },
-    type: "poison",
-    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/24.png",
-    name: "arbok",
-    base_experience: 157,
-    height: 35,
-    id: 24,
-    values: {
-      top: 5,
-      right: 9,
-      bottom: "A",
-      left: "A",
-    },
-  },
-  {
-    abilities: ["static", "lightning-rod"],
-    stats: {
-      hp: 35,
-      attack: 55,
-      defense: 40,
-      "special-attack": 50,
-      "special-defense": 50,
-      speed: 90,
-    },
-    type: "electric",
-    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-    name: "pikachu",
-    base_experience: 112,
-    height: 4,
-    id: 25,
-    values: {
-      top: 8,
-      right: "A",
-      bottom: 9,
-      left: 6,
-    },
-  },
-  {
-    abilities: ["overgrow", "chlorophyll"],
-    stats: {
-      hp: 45,
-      attack: 49,
-      defense: 49,
-      "special-attack": 65,
-      "special-defense": 65,
-      speed: 45,
-    },
-    type: "grass",
-    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    name: "bulbasaur",
-    base_experience: 64,
-    height: 7,
-    id: 1,
-    values: {
-      top: 8,
-      right: 4,
-      bottom: 2,
-      left: 7,
-    },
-  },
-  {
-    abilities: ["blaze", "solar-power"],
-    stats: {
-      hp: 39,
-      attack: 52,
-      defense: 43,
-      "special-attack": 60,
-      "special-defense": 50,
-      speed: 65,
-    },
-    type: "fire",
-    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-    name: "charmander",
-    base_experience: 62,
-    height: 6,
-    id: 4,
-    values: {
-      top: 7,
-      right: 6,
-      bottom: 1,
-      left: 4,
-    },
-  },
-];
+const firebaseConfig = {
+  apiKey: "AIzaSyCifnGRSy-BfeVJDIK0jPzTiFSMSBftJiQ",
+  authDomain: "pokemon-game-3bcbb.firebaseapp.com",
+  databaseURL: "https://pokemon-game-3bcbb-default-rtdb.firebaseio.com",
+  projectId: "pokemon-game-3bcbb",
+  storageBucket: "pokemon-game-3bcbb.appspot.com",
+  messagingSenderId: "784449797593",
+  appId: "1:784449797593:web:6846269046c314e1b5adc8"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
+database.ref('pokemons').once('value', (snapshot)=>{
+  console.log(snapshot.val())
+})
 
 const App = () => {
+  const match = useRouteMatch('/')
   return (
-    <>
-      <Header title="Header text" descr="Header description" />
-      <Layout title="Layout 1 text" urlBg={urlBG1} id="rules">
-        <p>
-          In the game two players face off against one another, one side playing
-          as "blue", the other as "red" on a 3x3 grid. Each player has five
-          cards in a hand and the aim is to capture the opponent's cards by
-          turning them into the player's own color of red or blue.
-        </p>
-        <p>
-          To win, a majority of the total ten cards played (including the one
-          card that is not placed on the board) must be of the player's card
-          color. To do this, the player must capture cards by placing a card
-          adjacent to an opponent's card whereupon the 'ranks' of the sides
-          where the two cards touch will be compared. If the rank of the
-          opponent's card is higher than the player's card, the player's card
-          will be captured and turned into the opponent's color. If the player's
-          rank is higher, the opponent's card will be captured and changed into
-          the player's color instead.
-        </p>
-      </Layout>
-      <Layout title="Layout 2 text" colorBg="red" id="cards">
-        <p>Some text...</p>
-        <p>Some text...</p>
-        <img className="image-logo" src={logoPng} alt="logo" />
-        <div className="flex">
-          {POKEMONS.map((item) => (
-            <PokemonCard key={item.id} id={item.id} name={item.name} img={item.img} type={item.type} values={item.values} />
-          ))}
-        </div>
-      </Layout>
-      <Layout title="Layout 3 text" urlBg={urlBG2} id="about">
-        <p>Some text...</p>
-        <p>Some text...</p>
-        <p>Some text...</p>
-      </Layout>
-      <Footer />
-    </>
+      <Switch>
+        <Route path="/404" render={() => <h1>404 Error</h1>} />
+        
+          <>
+            <MenuHeader bgActive={!match.isExact}/>
+            <div className={cn(s.wrap, {
+              [s.isHomePage]: match.isExact
+            })}>
+            <Switch>
+              <Route path="/" exact component={HomePage} />
+              <Route path="/home" component={HomePage} />
+              <Route path="/game" component={GamePage} />
+              <Route path="/about" render={() => <h1>ABOUT</h1>} />
+              <Route render={() => (
+                <Redirect to='./404' />
+              )}/>
+            </Switch>
+            </div>
+            <Footer />
+          </>
+        
+      </Switch>
   );
 };
 
--- pockemon-game/src/components/Header/index.js
+++ pockemon-game/src/components/Header/index.js
@@ -1,12 +1,18 @@
 import s from "./style.module.css";
+import { useHistory } from "react-router-dom";
 
-const Header = ({ title, descr }) => {
+const Header = ({ id, title, descr}) => {
+  const history = useHistory()
+  const handleClick = () => {
+    history.push('/game')
+  };
   return (
-    <header className={s.root}>
+    <header className={s.root} id={id}>
       <div className={s.forest}></div>
       <div className={s.container}>
         <h1>{title}</h1>
         <p>{descr}</p>
+        <button onClick={handleClick}>Start Game</button>
       </div>
     </header>
   );
